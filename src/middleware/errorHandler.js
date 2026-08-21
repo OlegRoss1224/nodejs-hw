6404d6ec4de5d883ed853eb3f99594e01597a832
OlegRoss1224 <olegross87@gmail.com>
2026-08-21 23:21:36 +0300

    Complete Homework 2 with MongoDB

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,14 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+      data: err.data,
+    });
+  }
+
+  res.status(500).json({
+    message: err.message || 'Something went wrong',
+  });
+};
