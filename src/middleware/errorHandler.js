cd93f47fa80021260b9d8cdad33bb01771221091
OlegRoss1224 <olegross87@gmail.com>
2026-08-22 00:26:33 +0300

    Fix errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,15 +1,13 @@
 import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  const status = err.status || err.statusCode || 500;
-
-  const message = err.message || err.name || 'Something went wrong';
-
-  if (err instanceof HttpError || err.status) {
-    return res.status(status).json({ message });
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
+    });
   }
 
-  return res.status(status).json({
-    message: status === 500 ? 'Something went wrong' : message,
+  res.status(500).json({
+    message: 'Something went wrong',
   });
 };
