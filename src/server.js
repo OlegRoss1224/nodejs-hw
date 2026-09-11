63749a83c6ac5d61d123499c674ca28caaca81f5
OlegRoss1224 <olegross87@gmail.com>
2026-09-11 23:02:42 +0300

    Fix middleware

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRouter from './routes/notesRoutes.js';
-import cookieParser from 'cookie-parser';
 import authRouter from './routes/authRoutes.js';
 
 dotenv.config();
@@ -20,15 +20,18 @@ export const startServer = async () => {
   app.use(logger);
   app.use(cors());
   app.use(express.json());
+  app.use(cookieParser());
 
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Server is running!' });
+  });
+
+  app.use('/auth', authRouter);
   app.use(notesRouter);
 
   app.use(errors());
   app.use(notFoundHandler);
   app.use(errorHandler);
-
-  app.use(cookieParser());
-  app.use('/auth', authRouter);
 
   try {
     await connectMongoDB();
